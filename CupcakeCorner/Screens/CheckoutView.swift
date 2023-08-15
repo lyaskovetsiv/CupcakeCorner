fd7ff6d465a279af25e9d912bc0bfd7ccc72125f
Ivan Lyaskovets <lyaskovets.iv@gmail.com>
2023-08-15 23:14:39 +0300

    Added alert message & URLSession&Request

--- CupcakeCorner/Screens/CheckoutView.swift
+++ CupcakeCorner/Screens/CheckoutView.swift
@@ -11,6 +11,9 @@ struct CheckoutView: View {
 	// MARK: - States&Properities
 
 	@ObservedObject private var order: Order
+	@State private var confirmationMessage = ""
+	@State private var showingConfirmation: Bool = false
+
 
 	// MARK: - Init
 
@@ -35,13 +38,53 @@ struct CheckoutView: View {
 				Text("Your total cost: \(order.cost, format: .currency(code: "USD"))")
 					.font(.title)
 
-				Button("Place order", action: {})
-					.padding()
+				Button("Place order") {
+					Task {
+						await placeOrder()
+					}
+				}
+				.padding()
 
 			}
 		}
 		.navigationTitle("Checkout")
 		.navigationBarTitleDisplayMode(.inline)
+		.alert("Thank you!", isPresented: $showingConfirmation) {
+			Button("OK!") {
+
+			}
+		} message: {
+			Text(confirmationMessage)
+		}
+	}
+
+	// MARK: - Methods
+
+	private func placeOrder() async {
+		guard let encoded = try? JSONEncoder().encode(order) else {
+			print("Failured to encode order")
+			return
+		}
+
+		guard let url = URL(string: "https://reqres.in/api/cupcakes") else {
+			print("Failured to get url from string")
+			return
+		}
+
+		var request = URLRequest(url: url)
+		request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+		request.httpMethod = "POST"
+
+		do {
+			let (data, _) = try await URLSession.shared.upload(for: request, from: encoded)
+
+			let decodedOrder = try JSONDecoder().decode(Order.self, from: data)
+			confirmationMessage = "Your order for \(decodedOrder.quantity)x\(Order.types[decodedOrder.type].lowercased()) cupcakes on its way!"
+			showingConfirmation = true
+
+		} catch {
+			print(error.localizedDescription)
+		}
 	}
 }
 
